71f5f32f628029b573c02805406da5538110dda6
Zamin <Muhammad.Zamin@pratian.com>
2025-04-04 19:22:13 +0530

    dynamic class and card-component dynamic

--- src/app/projects/modules/seeker/component/subcomponent/dashboard/home/home.component.ts
+++ src/app/projects/modules/seeker/component/subcomponent/dashboard/home/home.component.ts
@@ -21,4 +21,33 @@ export class HomeComponent implements OnInit {
     this.titleService.setTitle(newTitle);
   }
 
+
+  jobCardData = [
+    {
+      comanyLogo: 'assets/company_logo/cpmpany_logo1.png',
+      jobTitle: 'Software Engineer',
+      companyName: 'Tech Company',
+      location: 'New Delhi, India',
+      initialSalary: '20,000',
+      maxSalary: '60,000',
+      experience: 'Min 2 years',
+      jobType: 'Full Time',
+      matches: '80',
+      description: 'We are looking for a Software Engineer to join our team. You will be responsible for developing and maintaining software applications.'
+
+    },{
+      comanyLogo: 'assets/company_logo/cpmpany_logo1.png',
+      jobTitle: 'Software Engineer',
+      companyName: 'Tech Company',
+      location: 'New Delhi, India',
+      initialSalary: '20,000',
+      maxSalary: '60,000',
+      experience: 'Min 2 years',
+      jobType: 'Full Time',
+      matches: '80',
+      description: 'We are looking for a Software Engineer to join our team. You will be responsible for developing and maintaining software applications.'
+
+    },
+  ];
+
 }
--- src/app/projects/modules/seeker/shared/job-card/job-card.component.ts
+++ src/app/projects/modules/seeker/shared/job-card/job-card.component.ts
@@ -10,4 +10,34 @@ import { NgClass } from '@angular/common';
 })
 export class JobCardComponent {
   @Input() width: string = "w-full";
+  @Input() view: string = "grid";
+
+  @Input() jobCardData: JobCardData[] = [
+    {
+      comanyLogo: 'assets/company_logo/cpmpany_logo1.png',
+      jobTitle: 'Software Engineer',
+      companyName: 'Tech Company',
+      location: 'New Delhi, India',
+      initialSalary: '20,000',
+      maxSalary: '60,000',
+      experience: 'Min 2 years',
+      jobType: 'Full Time',
+      matches: '80',
+      description: 'We are looking for a Software Engineer to join our team. You will be responsible for developing and maintaining software applications.'
+
+    },
+  ];
+}
+
+interface JobCardData {
+  comanyLogo: string;
+  jobTitle: string;
+  companyName: string;
+  location: string;
+  initialSalary: string;
+  maxSalary: string;
+  experience: string;
+  matches: string;
+  jobType: string;
+  description: string;
 }
