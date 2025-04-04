f1bf33486646baabcb0f9bcec64f952786b1180b
Zamin <Muhammad.Zamin@pratian.com>
2025-04-04 13:45:37 +0530

    update my component

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    screens: {
+      'xs': '600',
+      'sm': '640px',
+      'md': '960px',
+      'lg': '1280px',
+      'xl': '1920px',
+    },
   },
   plugins: [],
 }
-
