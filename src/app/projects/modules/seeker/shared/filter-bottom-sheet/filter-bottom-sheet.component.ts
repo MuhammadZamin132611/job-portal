985dee41c67ef65b2048627ef583bae0e5e54bb9
Zamin <Muhammad.Zamin@pratian.com>
2025-04-07 19:12:07 +0530

    make it dynamic functionality for filter components

--- src/app/projects/modules/seeker/shared/filter-bottom-sheet/filter-bottom-sheet.component.ts
+++ src/app/projects/modules/seeker/shared/filter-bottom-sheet/filter-bottom-sheet.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MaterialModule } from '../../../../shared/material.module';
-import { MAT_BOTTOM_SHEET_DATA } from '@angular/material/bottom-sheet';
+import { MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef } from '@angular/material/bottom-sheet';
 import { NgIf } from '@angular/common';
 
 @Component({
@@ -11,10 +11,15 @@ import { NgIf } from '@angular/common';
 })
 export class FilterBottomSheetComponent {
   filterType: string = '';
-  constructor(@Inject(MAT_BOTTOM_SHEET_DATA) public data: { filterType: string }) 
+  constructor(@Inject(MAT_BOTTOM_SHEET_DATA) public data: { filterType: string },
+  private bottomSheetRef: MatBottomSheetRef<FilterBottomSheetComponent>,) 
   {
     this.filterType = data.filterType || 'Default';
     console.log("data.filterType",this.data)
+  }
+
+  closeSheet(): void {
+    this.bottomSheetRef.dismiss();
   }
   
 }
