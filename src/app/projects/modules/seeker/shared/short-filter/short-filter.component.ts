de798ee7fd42a1a2ca41dfd37ec6de763f120da0
Zamin <Muhammad.Zamin@pratian.com>
2025-04-07 18:34:00 +0530

    adding sort filter ui

--- src/app/projects/modules/seeker/shared/short-filter/short-filter.component.ts
+++ src/app/projects/modules/seeker/shared/short-filter/short-filter.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
+import { MaterialModule } from '../../../../shared/material.module';
 
 @Component({
   selector: 'app-short-filter',
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './short-filter.component.html',
   styleUrl: './short-filter.component.scss'
 })
 export class ShortFilterComponent {
 
+  filterList = [
+    { filterType: 'Experience Level', model: true },
+    { filterType: 'Freshness', model: true },
+    { filterType: 'Job Roles ', model: true },
+    { filterType: 'Work From Home ', model: false },
+    { filterType: 'Full Time', model: false },
+    { filterType: 'Part Time', model: false },
+    { filterType: 'Job City', model: true },
+    { filterType: 'No English', model: false },
+    { filterType: 'Night Shift', model: false },
+    { filterType: 'Day Shift', model: false },
+    { filterType: 'Salary', model: true },
+  ];
 }
