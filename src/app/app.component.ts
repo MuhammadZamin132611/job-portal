c77cdfd5651c33837bebb2753a31a69a06a032c9
Zamin <Muhammad.Zamin@pratian.com>
2025-05-06 13:11:08 +0530

    offline screen added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -19,5 +20,34 @@ export class AppComponent implements OnInit {
 
   setTitle(newTitle: string) {
     this.titleService.setTitle(newTitle);
+  }
+
+  isOnline = navigator.onLine;
+
+  @HostListener('window:online')
+  onOnline() {
+    this.isOnline = true;
+  }
+
+  @HostListener('window:offline')
+  onOffline() {
+    this.isOnline = false;
+  }
+
+  openNetworkSettings() {
+    const ua = navigator.userAgent || navigator.vendor;
+
+    // On Android, attempt to redirect to settings via intent
+    if (/android/i.test(ua)) {
+      window.location.href = 'intent:#Intent;action=android.settings.WIFI_SETTINGS;end';
+    }
+    // On iOS, suggest opening settings
+    else if (/iPad|iPhone|iPod/.test(ua)) {
+      alert('Please open Settings > Wi-Fi to check your connection.');
+    }
+    // On desktop or unknown platform
+    else {
+      alert('Please check your network settings manually.');
+    }
   }
 }
