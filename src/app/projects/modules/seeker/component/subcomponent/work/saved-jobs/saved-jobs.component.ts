207c315df3bf97dec24475abdc376167e23ba558
Zamin <Muhammad.Zamin@pratian.com>
2025-05-06 18:58:40 +0530

    adding saved jobs component

--- src/app/projects/modules/seeker/component/subcomponent/work/saved-jobs/saved-jobs.component.ts
+++ src/app/projects/modules/seeker/component/subcomponent/work/saved-jobs/saved-jobs.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { MaterialModule } from '../../../../../../shared/material.module';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-saved-jobs',
-  imports: [],
+  imports: [MaterialModule, NgClass],
   templateUrl: './saved-jobs.component.html',
   styleUrl: './saved-jobs.component.scss'
 })
@@ -17,4 +19,50 @@ export class SavedJobsComponent {
   setTitle(newTitle: string) {
     this.titleService.setTitle(newTitle);
   }
+  @Input() width: string = "w-full";
+  @Input() view: string = "grid";
+
+  @Input() appliedJobData: JobCardData[] = [
+    {
+      comanyLogo: 'assets/company_logo/cpmpany_logo1.png',
+      jobTitle: 'Software Engineer',
+      companyName: 'JCS Company',
+      location: 'New Delhi, India',
+      initialSalary: '20,000',
+      maxSalary: '60,000',
+      matches: '85',
+      saved:"Saved 1 week ago"
+    },
+    {
+      comanyLogo: 'assets/company_logo/cpmpany_logo2.png',
+      jobTitle: 'Mobile Engineer',
+      companyName: 'Tech Company',
+      location: 'New Delhi, India',
+      initialSalary: '40,000',
+      maxSalary: '80,000',
+      matches: '90',
+      saved:"Saved 2 week ago"
+    },
+    {
+      comanyLogo: 'assets/company_logo/cpmpany_logo3.png',
+      jobTitle: 'Testing Engineer',
+      companyName: 'Nova Company',
+      location: 'New Delhi, India',
+      initialSalary: '80,000',
+      maxSalary: '1,20,000',
+      matches: '95',
+      saved:"Saved 3 week ago"
+    },
+  ];
+}
+
+interface JobCardData {
+  comanyLogo: string;
+  jobTitle: string;
+  companyName: string;
+  location: string;
+  initialSalary: string;
+  maxSalary: string;
+  matches: string;
+  saved:string
 }
