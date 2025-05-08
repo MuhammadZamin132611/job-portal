a0ff5e4e19c1940f2017fb02d683e5e2be8de1e7
Zamin <Muhammad.Zamin@pratian.com>
2025-05-08 15:02:35 +0530

    adding half part of job details components UI

--- src/app/projects/modules/seeker/component/subcomponent/dashboard/job-details/job-details.component.ts
+++ src/app/projects/modules/seeker/component/subcomponent/dashboard/job-details/job-details.component.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { MaterialModule } from '../../../../../../shared/material.module';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-job-details',
-  imports: [],
+  imports: [MaterialModule, NgClass],
   templateUrl: './job-details.component.html',
   styleUrl: './job-details.component.scss'
 })
 export class JobDetailsComponent {
+  activeSection: string = 'details';
+
+  scrollToSection(sectionId: string): void {
+    const el = document.getElementById(sectionId);
+    if (el) {
+      el.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      this.activeSection = sectionId;
+    }
+  }
+
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    const sections = ['details', 'insights', 'company', 'salary'];
+    for (let section of sections) {
+      const el = document.getElementById(section);
+      if (el) {
+        const rect = el.getBoundingClientRect();
+        if (rect.top <= 100 && rect.bottom >= 100) {
+          this.activeSection = section;
+          break;
+        }
+      }
+    }
+  }
+
 
 }
