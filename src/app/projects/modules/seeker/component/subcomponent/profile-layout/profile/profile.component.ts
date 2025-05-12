afdf30e27ae34796c6343b78b79f960c90efcf42
Zamin <Muhammad.Zamin@pratian.com>
2025-05-12 17:03:47 +0530

    adding work-experienc component UI

--- src/app/projects/modules/seeker/component/subcomponent/profile-layout/profile/profile.component.ts
+++ src/app/projects/modules/seeker/component/subcomponent/profile-layout/profile/profile.component.ts
@@ -3,10 +3,11 @@ import { Title } from '@angular/platform-browser';
 import { BasicDetailsComponent } from "../basic-details/basic-details.component";
 import { AboutMeComponent } from "../about-me/about-me.component";
 import { JobPrefrenceComponent } from "../job-prefrence/job-prefrence.component";
+import { WorkExperienceComponent } from "../work-experience/work-experience.component";
 
 @Component({
   selector: 'app-profile',
-  imports: [BasicDetailsComponent, AboutMeComponent, JobPrefrenceComponent],
+  imports: [BasicDetailsComponent, AboutMeComponent, JobPrefrenceComponent, WorkExperienceComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
--- src/app/projects/modules/seeker/component/subcomponent/profile-layout/work-experience/work-experience.component.ts
+++ src/app/projects/modules/seeker/component/subcomponent/profile-layout/work-experience/work-experience.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../../../../../shared/material.module';
 
 @Component({
   selector: 'app-work-experience',
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './work-experience.component.html',
   styleUrl: './work-experience.component.scss'
 })
