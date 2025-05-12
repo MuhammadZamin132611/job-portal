0e080d106c66bf212f7f2b32f224e986afd3d2e3
Zamin <Muhammad.Zamin@pratian.com>
2025-05-12 14:48:04 +0530

    about-me component ui added

---
+++ src/app/projects/modules/seeker/component/subcomponent/profile-layout/about-me/about-me.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MaterialModule } from '../../../../../../shared/material.module';
+
+@Component({
+  selector: 'app-about-me',
+  imports: [MaterialModule],
+  templateUrl: './about-me.component.html',
+  styleUrl: './about-me.component.scss'
+})
+export class AboutMeComponent {
+
+}
--- src/app/projects/modules/seeker/component/subcomponent/profile-layout/profile/profile.component.ts
+++ src/app/projects/modules/seeker/component/subcomponent/profile-layout/profile/profile.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { BasicDetailsComponent } from "../basic-details/basic-details.component";
+import { AboutMeComponent } from "../about-me/about-me.component";
 
 @Component({
   selector: 'app-profile',
-  imports: [BasicDetailsComponent],
+  imports: [BasicDetailsComponent, AboutMeComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
