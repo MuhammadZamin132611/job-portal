581396a9eec0be7b2c2210df7c5d63e1e101dfd7
Zamin <Muhammad.Zamin@pratian.com>
2025-05-12 19:16:14 +0530

    adding achievemnts component UI

--- src/app/projects/modules/seeker/component/subcomponent/profile-layout/basic-details/basic-details.component.ts
+++ src/app/projects/modules/seeker/component/subcomponent/profile-layout/basic-details/basic-details.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../../../../../../shared/material.module';
-import { ProfileComponent } from "../profile/profile.component";
-import { ProfileImageComponent } from "../../../../shared/profile-image/profile-image.component";
 
 @Component({
   selector: 'app-basic-details',
-  imports: [MaterialModule, ProfileImageComponent],
+  imports: [MaterialModule],
   templateUrl: './basic-details.component.html',
   styleUrl: './basic-details.component.scss'
 })
