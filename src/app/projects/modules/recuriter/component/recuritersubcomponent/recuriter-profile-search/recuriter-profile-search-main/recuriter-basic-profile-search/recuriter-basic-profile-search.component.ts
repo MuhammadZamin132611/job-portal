6db1671bf2c2055a482e30089fee8a0fde5fcd28
Zamin <Muhammad.Zamin@pratian.com>
2025-05-31 15:38:56 +0530

    update bottom sheet checkbox

--- src/app/projects/modules/recuriter/component/recuritersubcomponent/recuriter-profile-search/recuriter-profile-search-main/recuriter-basic-profile-search/recuriter-basic-profile-search.component.ts
+++ src/app/projects/modules/recuriter/component/recuritersubcomponent/recuriter-profile-search/recuriter-profile-search-main/recuriter-basic-profile-search/recuriter-basic-profile-search.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MaterialModule } from '../../../../../../../shared/material.module';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { MasterDateBottomSheetComponent } from '../../../../../../../shared/master-date-bottom-sheet/master-date-bottom-sheet.component';
+import { Skills } from '../../../../../../../../data/skills';
 
 @Component({
   selector: 'app-recuriter-basic-profile-search',
@@ -13,8 +16,11 @@ export class RecuriterBasicProfileSearchComponent implements OnInit {
   noticePeriod = ["Immediate Joining", "15 Days or Less ", "1 Month or Less", "2 Month or Less", "3 Month or Less",];
   educationType = ["Part Time", "Full Time ",];
   basicSearch!: FormGroup;
-
-  constructor(private fb: FormBuilder) { }
+  private _bottomSheet = inject(MatBottomSheet);
+  skill: string[] = [];
+  constructor(private fb: FormBuilder) {
+    this.skill = Skills;
+  }
 
   ngOnInit(): void {
     this.basicSearch = this.fb.group({
@@ -53,7 +59,28 @@ export class RecuriterBasicProfileSearchComponent implements OnInit {
       this.basicSearch.markAllAsTouched(); // Show all error messages
       console.log("Invalid Form", this.basicSearch.value)
     }
+  }
 
+
+  selectedDegree: string = ''
+  openBottomSheetDegree(filterType: string[]): void {
+    const bottomSheetRef = this._bottomSheet.open(MasterDateBottomSheetComponent, {
+      panelClass: 'custom-bottom-sheet',
+      data: {
+        filterType,
+        selected: this.selectedDegree,
+        typeData: 'Skills',
+        placeholderMessage: 'Java, Angular, Puthon, React'
+      }
+    });
+    bottomSheetRef.afterDismissed().subscribe((selectedRole: string) => {
+      if (selectedRole !== undefined) {
+        // console.log('Selected Roles:', selectedRole);
+        this.selectedDegree = selectedRole;
+        this.basicSearch.get('skills')?.setValue(selectedRole);
+        // this.workExperience.get('degree')?.markAsTouched();
+      }
+    });
   }
 
 
--- src/app/projects/shared/master-date-bottom-sheet/master-date-bottom-sheet.component.ts
+++ src/app/projects/shared/master-date-bottom-sheet/master-date-bottom-sheet.component.ts
@@ -2,12 +2,12 @@ import { Component, EventEmitter, Inject, Output } from '@angular/core';
 import { MaterialModule } from '../material.module';
 import { Role } from '../../../data/role';
 import { location } from '../../../data/locations';
-import { NgIf } from '@angular/common';
+import { NgClass, NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MAT_BOTTOM_SHEET_DATA, MatBottomSheetRef } from '@angular/material/bottom-sheet';
 @Component({
   selector: 'app-master-date-bottom-sheet',
-  imports: [MaterialModule, NgIf, FormsModule,],
+  imports: [MaterialModule, NgIf, FormsModule, NgClass],
   templateUrl: './master-date-bottom-sheet.component.html',
   styleUrl: './master-date-bottom-sheet.component.scss'
 })
@@ -17,16 +17,16 @@ export class MasterDateBottomSheetComponent {
   allData: string[] = [];
   filterType: string = '';
 
-  constructor(@Inject(MAT_BOTTOM_SHEET_DATA) public data: { filterType: string[], selected: string[] },
+  constructor(@Inject(MAT_BOTTOM_SHEET_DATA) public data: { filterType: string[], selected: string[], typeData: string, placeholderMessage: string },
     private bottomSheetRef: MatBottomSheetRef<MasterDateBottomSheetComponent>,) {
     this.allData = this.data.filterType;
     this.selected = [...(this.data.selected || [])];
   }
+
   select(role: string): void {
     const index = this.selected.indexOf(role);
     if (index === -1) {
       this.selected.push(role);
-      this.closeSheet();
     } else {
       // this.selected.splice(index, 1);
       this.remove(role);
@@ -41,6 +41,13 @@ export class MasterDateBottomSheetComponent {
     }
   }
 
+  displaySelected: string[] = [];
+  selectedData() {
+    this.closeSheet();
+    console.log(this.selected)
+    this.displaySelected = [...this.selected];
+  }
+
   filter(): string[] {
     const term = this.search.toLowerCase();
     if (!Array.isArray(this.allData)) {
@@ -52,5 +59,9 @@ export class MasterDateBottomSheetComponent {
 
   closeSheet(): void {
     this.bottomSheetRef.dismiss(this.selected); // Send selected data to parent
+  }
+  
+  closeSheetDataNotSahred(): void {
+    this.bottomSheetRef.dismiss();
   }
 }
